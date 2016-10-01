2936be5dca7005e059e136115dc9e3942ab23a95
Trevor Murphy <murphtrevor@gmail.com>
2016-10-01 18:44:41 -0500

    Working on assign2 task1

--- assign2/assign2.scm
+++ assign2/assign2.scm
@@ -10,23 +10,35 @@
                 " (it should be " target ")")))
 
 ; Task 1
-(define (iterate # $iterable)
-    (lambda (i)
-        ; bind each value in iterable to i
+(define (iterate # $i $lyst $)
+    (define lyst (eval $lyst #))
+    (define $i (eval (car lyst) #))
+    (println "the first argument is " $i)
+    (println "the second argument is " $lyst)
+    (println "the remaining arguments are:")
+    (while (valid? $)
+        (eval (car $) #)
+        (set! $ (cdr $))
         )
-    ; pre
-    (define i (get $i #))
-    (define iterable (get $iterable #))
+    )
 
-    ; body
+    ; (eval lambda #) ??? 'evaluate it in the calling environment'
+    ;(define lyst (eval $lyst #))
+    ;(cond
+    ;    ((null? lyst))
+    ;    (else
+    ;        ((lambda (lyst)
+    ;            (define i (eval (car lyst) #))
+    ;            (define inspect1 (eval $inspect1 #))
+    ;            (define inspect2 (eval $inspect2 #))) lyst)
+    ;        (iterate $i (cdr lyst) $inspect1 $inspect2))))
 
-    ; post
-    (set $i i #)
-    (set $iterable iterable #))
+(define (testIter lyst)
+    (cond
+        ((null? lyst))
+        (else (inspect (car lyst)) (testIter (cdr lyst)))))
 
 (define (run1)
-    (iterate i (list 1 2 3 4)
-        (inspect i)
-        (inspect (* i i))))
+    (iterate i (list 1 2 3 4) (inspect i) (inspect (* i i))))
 
 (run1)
