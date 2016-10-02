9cea64246508e649d84e844a3601fbcba0c40211
Trevor Murphy <murphtrevor@gmail.com>
2016-10-01 22:52:27 -0500

    Finish task 8.

--- assign2/assign2.scm
+++ assign2/assign2.scm
@@ -41,4 +41,26 @@
 (define (run1)
     (iterate i (list 1 2 3 4) (inspect i) (inspect (* i i))))
 
+; Task 2
+; Task 3
+; Task 4
+; Task 5
+; Task 6
+; Task 7
+
+; Task 8
+(define (cxr sym)
+    (define (helper x sym)
+        (define char (car (string sym)))
+        (cond
+            ((equal? (string sym) "nil") x)
+            ((equal? char "a") (car (helper x (cdr (string sym)))))
+            ((equal? char "d") (cdr (helper x (cdr (string sym)))))))
+    (lambda (x) (helper x sym)))
+
+(define (run8)
+    (inspect ((cxr 'dad) '(1 2 3 4 5 6))))
+; Task 9
+; Task 10
+
 (run1)
