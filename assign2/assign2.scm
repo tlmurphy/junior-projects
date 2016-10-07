78c3488c0d393ff21804fb9ab404533ac569dbf5
Trevor Murphy <murphtrevor@gmail.com>
2016-10-06 19:46:46 -0500

    Finish task 2

--- assign2/assign2.scm
+++ assign2/assign2.scm
@@ -1,6 +1,11 @@
+; Assignment 2
+; CS 403 - Programming Languages
+; Written by Trevor Murphy
+
 (define (author)
         (println "Trevor Murphy tmurphy2@crimson.ua.edu"))
 
+; For testing purposes
 (define (exprTest # $expr target)
         (define result (catch (eval $expr #)))
         (if (error? result)
@@ -20,6 +25,41 @@
     (iterate i (list 1 2 3 4) (inspect i) (inspect (* i i))))
 
 ; Task 2
+(define (peval f @)
+    (define args @)
+    (define returnArgs (list))
+    (define (replaceMissing args largs returnArgs)
+        (if (equal? (string args) "nil")
+            returnArgs
+            (if (equal? 'MISSING (car args))
+                (replaceMissing (cdr args) (cdr largs) (append returnArgs (list (car largs))))
+                (replaceMissing (cdr args) largs (append returnArgs (list (car args)))))))
+    (lambda (@) (apply f (replaceMissing args @ returnArgs))))
+
+(define (run2)
+    (define . 'MISSING)
+    (define (f x y z) (+ x y z))
+    (define (g a b c d e f g h i j k)
+        (+ (- (+ (* k j) i) h) g f e d c b a))
+    (define (h)
+        5)
+    (define (i a)
+        (* a a))
+    (exprTest (f 1 2 3) 6)
+    (exprTest ((peval f 1 2 3)) 6)
+    (exprTest ((peval f . . .) 1 2 3) 6)
+    (exprTest ((peval f . 2 .) 1 3) 6)
+    (exprTest ((peval f . . 3) 1 2) 6)
+    (exprTest ((peval f 1 . 3) 2) 6)
+    (exprTest ((peval f 1 2 3)) 6)
+    (exprTest ((peval g 1 . 3 4 5 12 13 . 23 . .) 2 14 1 2) 51)
+    (exprTest ((peval g 1 2 . 4 5 12 . 14 23 1 2) 3 13) 51)
+    (exprTest ((peval g . . . 4 5 12 13 14 23 . .) 1 2 3 1 2) 51)
+    (exprTest (h) 5)
+    (exprTest ((peval h)) 5)
+    (exprTest ((peval i 4)) 16)
+    (exprTest ((peval i .) 4) 16))
+
 ; Task 3
 ; Task 4
 ; Task 5
@@ -42,4 +82,4 @@
 ; Task 9
 ; Task 10
 
-(run8)
+(run2)
