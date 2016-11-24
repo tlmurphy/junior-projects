2f66ad33e1730d55813c0b95ca972b8fd023e65e
Trevor Murphy <murphtrevor@gmail.com>
2016-11-23 23:53:09 -0600

    finish task 4

--- assign3/assign3.scm
+++ assign3/assign3.scm
@@ -113,6 +113,185 @@
 
 ;===================================Task 4======================================
 
+(define true #t)
+(define false #f)
+
+(define (has-value? connector)
+  (connector 'has-value?))
+
+(define (get-value connector)
+  (connector 'value))
+
+(define (set-value! connector new-value informant)
+  ((connector 'set-value!) new-value informant))
+
+(define (forget-value! connector retractor)
+  ((connector 'forget) retractor))
+
+(define (connect connector new-constraint)
+  ((connector 'connect) new-constraint))
+
+(define (for-each-except exception procedure list)
+    (define (loop items)
+      (cond ((null? items) 'done)
+            ((eq? (car items) exception) (loop (cdr items)))
+            (else (procedure (car items))
+                  (loop (cdr items)))))
+    (loop list))
+
+(define (memq item x)
+    (cond
+        ((null? x) false)
+        ((eq? item (car x)) true)
+        (else
+            (memq item (cdr x)))))
+
+(define (inform-about-value constraint)
+  (constraint 'I-have-a-value))
+
+(define (inform-about-no-value constraint)
+  (constraint 'I-lost-my-value))
+
+(define (multiplier m1 m2 product)
+    (define (process-new-value)
+      (cond ((or (and (has-value? m1) (= (get-value m1) 0))
+                 (and (has-value? m2) (= (get-value m2) 0)))
+             (set-value! product 0 me))
+            ((and (has-value? m1) (has-value? m2)) ; m1 * m2 = product
+             (set-value! product
+                         (* (get-value m1) (get-value m2))
+                         me))
+            ((and (has-value? product) (has-value? m1)) ; product / m1 = m2
+             (set-value! m2
+                         (/ (real (get-value product)) (get-value m1))
+                         me))
+            ((and (has-value? product) (has-value? m2)) ; product / m2 = m1
+             (set-value! m1
+                         (/ (real (get-value product)) (get-value m2))
+                         me))))
+    (define (process-forget-value)
+      (forget-value! product me)
+      (forget-value! m1 me)
+      (forget-value! m2 me)
+      (process-new-value))
+    (define (me request)
+      (cond ((eq? request 'I-have-a-value)
+             (process-new-value))
+            ((eq? request 'I-lost-my-value)
+             (process-forget-value))
+            (else
+             (error "Unknown request - MULTIPLIER" request))))
+    (connect m1 me)
+    (connect m2 me)
+    (connect product me)
+    me)
+
+
+
+(define (divider m1 m2 result)
+    (define (process-new-value)
+      (cond ((or (and (has-value? m1) (= (get-value m1) 0))
+                 (and (has-value? m2) (= (get-value m2) 0)))
+             (set-value! result 0 me))
+            ((and (has-value? m1) (has-value? m2)) ; m1 / m2 = result
+             (set-value! result
+                         (/ (real (get-value m1)) (get-value m2))
+                         me))
+            ((and (has-value? result) (has-value? m1)) ; m1 / result = m2
+             (set-value! m2
+                         (/ (real (get-value m1)) (get-value result))
+                         me))
+            ((and (has-value? result) (has-value? m2)) ; m2 * result = m1
+             (set-value! m1
+                         (* (get-value result) (get-value m2))
+                         me))))
+    (define (process-forget-value)
+      (forget-value! result me)
+      (forget-value! m1 me)
+      (forget-value! m2 me)
+      (process-new-value))
+    (define (me request)
+      (cond ((eq? request 'I-have-a-value)
+             (process-new-value))
+            ((eq? request 'I-lost-my-value)
+             (process-forget-value))
+            (else
+             (error "Unknown request - DIVIDER" request))))
+    (connect m1 me)
+    (connect m2 me)
+    (connect result me)
+    me)
+
+(define (constant value connector)
+  (define (me request)
+    (error "Unknown request - CONSTANT" request))
+  (connect connector me)
+  (set-value! connector value me)
+  me)
+
+
+(define (make-connector)
+    (let ((value false) (informant false) (constraints '()))
+      (define (set-my-value newval setter)
+        (cond ((not (has-value? me))
+               (set! value newval)
+               (set! informant setter)
+               (for-each-except setter
+                                inform-about-value
+                                constraints))
+              ((not (= value newval))
+               (error "Contradiction" (list value newval)))
+              (else 'ignored)))
+      (define (forget-my-value retractor)
+        (if (eq? retractor informant)
+            (begin (set! informant false)
+                   (for-each-except retractor
+                                    inform-about-no-value
+                                    constraints))
+            'ignored))
+      (define (connect new-constraint)
+        (if (not (memq new-constraint constraints))
+            (set! constraints
+                  (cons new-constraint constraints)))
+        (if (has-value? me)
+            (inform-about-value new-constraint))
+        'done)
+      (define (me request)
+        (cond ((eq? request 'has-value?)
+               (if (not (eq? informant false)) true false)) ; Had to change this from the book
+              ((eq? request 'value) value)
+              ((eq? request 'set-value!) set-my-value)
+              ((eq? request 'forget) forget-my-value)
+              ((eq? request 'connect) connect)
+              (else (error "Unknown operation - CONNECTOR"
+                           request))))
+      me))
+
+(define (gravity f m1 m2 r)
+    (let ((mult     (make-connector))
+          (r-square (make-connector))
+          (divided  (make-connector))
+          (g        (make-connector)))
+
+      (multiplier m1 m2 mult)
+      (multiplier r r r-square)
+      (divider mult r-square divided)
+      (multiplier g divided f)
+      (constant 0.00667300 g)
+      'ok))
+
+(define (run4)
+    (define f (make-connector))
+    (define m1 (make-connector))
+    (define m2 (make-connector))
+    (define r (make-connector))
+    (gravity f m1 m2 r)
+    (set-value! m1 25 this)
+    (set-value! m2 4 this)
+    (set-value! r 4 this)
+    (inspect (get-value f)))
+
+
 ;===================================Task 5======================================
 
 ;===================================Task 6======================================
@@ -134,11 +313,12 @@
 
 (define (divisible? x y) (= (remainder x y) 0))
 
+(define (integers-starting-from n)
+    (if (or (seven-primes n) (eleven-primes n))
+        (scons n (integers-starting-from (+ n 1)))
+        (integers-starting-from (+ n 1))))
+
 (define (big-gulp)
-    (define (integers-starting-from n)
-        (if (or (seven-primes n) (eleven-primes n))
-            (scons n (integers-starting-from (+ n 1)))
-            (integers-starting-from (+ n 1))))
     (integers-starting-from 7))
 
 (define (seven-primes n)
@@ -251,8 +431,55 @@
     (stream-display (ps-mystery 1) 5)
     (stream-display (acc-mystery 1) 5)
     (stream-display (super-mystery 1) 5))
-    
+
 
 ;===================================Task 9======================================
 
+(define (merge-weighted s1 s2 weight)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+          (let ((s1carweight (weight (stream-car s1)))
+                (s2carweight (weight (stream-car s2))))
+            (cond ((> s1carweight s2carweight)
+                   (cons-stream (stream-car s2)
+                                (merge-weighted s1 (stream-cdr s2) weight)))
+                  ((< s1carweight s2carweight)
+                   (cons-stream (stream-car s1)
+                                (merge-weighted (stream-cdr s1) s2 weight)))
+                  (else
+                    (cons-stream (stream-car s1)
+                                 (cons-stream (stream-car s2)
+                                              (merge-weighted (stream-cdr s1) (stream-cdr s2) weight)))))))))
+
+(define (weighted-pairs s t weight)
+  (cons-stream (list (stream-car s) (stream-car t))
+               (merge-weighted
+                 (stream-map (lambda (x) (list (stream-car s) x))
+                             (stream-cdr t))
+                 (weighted-pairs (stream-cdr s)
+                                 (stream-cdr t)
+                                 weight)
+                 weight)))
+
+(define (cube-sum pair)
+    (let ((i (car pair))
+          (j (cadr pair)))
+      (+ (* i i i) (* j j j))))
+
+;(define sortedStream (weighted-pairs cube-sum (integers-starting-from 0)
+                                            ;  (integers-starting-from 0)))
+
+(define (ramanujan s pre)
+    (let ((num (cubicSum (stream-car s))))
+        (cond ((= pre num)
+                   (cons-stream num (ramanujan (stream-cdr s) num)))
+              (else (ramanujan (stream-cdr s) num)))))
+
+;(display-stream (ramanujan sortedStream 0))
+
+(define (run9)
+    (stream-display (ramujan-numbers) 5))
+
+;(run9)
 (println "assignment 3 loaded!")
